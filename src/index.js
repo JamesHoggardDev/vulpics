006897f5165a86d209f7242f3d6efbc902a03058
James <Jameshoggard.dev@gmail.com>
2022-01-27 18:20:41 -0500

    upyra3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 
-let imgCont = document.querySelector("#fox-image-container");
-let speciesUl = document.querySelector("#fox-species");
+const imgCont = document.querySelector("#fox-image-container");
+const speciesUl = document.querySelector("#fox-species");
 const imgUrl = "https://randomfox.ca/floof/";
-
 
 fetch("https://randomfox.ca/floof/")
   .then((resp) => resp.json())
@@ -14,16 +13,22 @@ function renderOneImg(foxObj) {
   const imgDiv = document.createElement("div");
   const foxFrame = document.createElement("img");
   foxFrame.src = foxObj.image;
-
   imgDiv.append(foxFrame);
   imgCont.append(imgDiv);
-}
+};
 
 fetch('http://localhost:3000/species')
   .then((resp) => resp.json())
   .then((specArr) => {
-      specArr.forEach((specObj) => {
-        renderOneSpec(specObj)
-      })
+    specArr.forEach((specObj) => {
+      renderAllSpecs(specObj);
+    })
   });
-
+  
+function renderAllSpecs(specObj) {
+  const specLi = document.createElement('li');
+  specLi.dataset.id = specObj.id;
+  specLi.textContent = specObj.title;
+  speciesUl.append(specLi);
+}
+renderAllSpecs();
